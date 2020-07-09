0621bdda83951dfc7da568ad3b13440600f79a0a
Linxiao Zeng <linxiao.zeng@gmail.com>
2020-07-09 16:52:42 +0200

    fix gpu_rank for cpu

--- onmt/trainer.py
+++ onmt/trainer.py
@@ -50,7 +50,7 @@ def build_trainer(opt, device_id, model, fields, optim, model_saver=None):
     if device_id >= 0:
         gpu_rank = opt.gpu_ranks[device_id]
     else:
-        gpu_rank = 0
+        gpu_rank = -1
         n_gpu = 0
     gpu_verbose_level = opt.gpu_verbose_level
 
@@ -83,7 +83,7 @@ def build_trainer(opt, device_id, model, fields, optim, model_saver=None):
                            n_gpu, gpu_rank,
                            gpu_verbose_level, report_manager,
                            with_align=True if opt.lambda_align > 0 else False,
-                           model_saver=model_saver if gpu_rank == 0 else None,
+                           model_saver=model_saver if gpu_rank <= 0 else None,
                            average_decay=average_decay,
                            average_every=average_every,
                            model_dtype=opt.model_dtype,
